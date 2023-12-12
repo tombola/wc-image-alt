4bddaa251e2a81391551d05fe0051c530e55b2fc
tombola <tombola@github>
2023-12-12 15:58:19 +0000

    Import CSV and update WC alt tags

--- src/wc_image_alt/cli/__init__.py
+++ src/wc_image_alt/cli/__init__.py
@@ -6,6 +6,7 @@ import os
 import requests
 import csv
 from functools import wraps
+from collections import defaultdict
 
 from rich.console import Console
 from rich.table import Table
@@ -18,6 +19,17 @@ WC_CONSUMER_KEY = os.environ.get("WC_CONSUMER_KEY")
 WC_CONSUMER_SECRET = os.environ.get("WC_CONSUMER_SECRET")
 WC_PRODUCTION_ENVIRONMENT = os.environ.get("WC_PRODUCTION_ENVIRONMENT")
 
+
+ALT_CSV_HEADINGS = (
+    "Product name",
+    "Image name",
+    "Alt",
+    "Suggested",
+    "Src",
+    "Product",
+    "Product ID",
+    "Image ID",
+)
 
 WC_MAX_API_RESULT_COUNT = 100
 CSV_OUTPUT_FILE = "product_images.csv"
@@ -35,7 +47,7 @@ def get_wcapi() -> API:
     ):
         console.print("Credentials not provided from environment")
         quit()
-    console.print("Connecting to {WC_URL}")
+    console.print(f"Connecting to {WC_URL}")
     return API(
         url=WC_URL,
         consumer_key=WC_CONSUMER_KEY,
@@ -45,9 +57,11 @@ def get_wcapi() -> API:
     )
 
 
-def wcapi(func):
+def wc_api(func):
     def wrapper(*args, **kwargs):
-        return func(wcapi=get_wcapi(), *args, **kwargs)
+        # Add wcapi to positional args to avoid confusing python
+        # https://stackoverflow.com/questions/35383767
+        return func(*((get_wcapi(),) + args), **kwargs)
 
     return wrapper
 
@@ -84,7 +98,7 @@ def aggregate_paginated_response(func):
     return wrapper
 
 
-@wcapi
+@wc_api
 @aggregate_paginated_response
 def get_all_products(wcapi: API, page=1) -> requests.Response:
     """
@@ -112,11 +126,10 @@ def get_alt_suggestion(product):
     hyphenated = alt.split(" - ")
     if len(hyphenated) == 2:
         alt = f"{hyphenated[1]}{hyphenated[0]}"
-    # strip quotes to avoid issues?
     return alt
 
 
-@wcapi
+@wc_api
 def get_products(wcapi: API, num=0) -> dict:
     """
     Query WooCommerce rest api for specified number of products
@@ -133,15 +146,30 @@ def get_products(wcapi: API, num=0) -> dict:
     return response.json()
 
 
+@wc_api
+def wc_update_product_images(wcapi: API, product_id, product_images):
+    console.print(product_images)
+
+    response = wcapi.post(
+        f"products/{product_id}",
+        data={"images": product_images},
+    )
+    console.print(response.status_code)
+    response.raise_for_status()
+    console.print(response.json()["images"])
+
+
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.option("--verbose", "-v", is_flag=True)
 @click.option("--force", "-f", is_flag=True)
 @click.option("--write", "-w", is_flag=True)
 @click.version_option(version=__version__, prog_name="wc-image-alt")
 @click.pass_context
-def cli(ctx, write, force):
+def cli(ctx, write, force, verbose):
     ctx.ensure_object(dict)
     ctx.obj["write"] = write
     ctx.obj["force"] = force
+    ctx.obj["verbose"] = verbose
 
 
 @cli.command()
@@ -209,3 +237,55 @@ def export_all(ctx, rows, *args, **kwargs):
     console.print(table)
     console.print(f"{len(products)} products returned")
     console.print(f"Exported to {CSV_OUTPUT_FILE}")
+
+
+@cli.command()
+@click.option("-n", "--rows", type=click.INT, default=0)
+@click.pass_context
+def import_csv(ctx, rows, *args, **kwargs):
+    force = ctx.parent.obj["force"]
+    write = ctx.parent.obj["write"]
+    verbose = ctx.parent.obj["verbose"]
+
+    console.log(f"{force=} {write=}")
+
+    highlight = ""
+    if WC_PRODUCTION_ENVIRONMENT:
+        force = False
+        highlight = "[red bold]"
+
+    console.print(f"\nUpdating {highlight}{WC_URL}")
+    if force or click.confirm('continue?'):
+        click.echo("Getting products from WooCommerce")
+    else:
+        click.echo("Goodbye")
+        exit(1)
+
+    with open(CSV_OUTPUT_FILE, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        all_products_images = defaultdict(lambda: [])
+
+        for i, row in enumerate(csv_reader):
+            if i == 0:
+                print(f'Column names are {", ".join(row)}')
+                continue
+            if rows and i == rows:
+                # Number of rows limit
+                console.print(f'[bold magenta]Processed {i} rows')
+                break
+            product__id = row["Product ID"]
+            image__id = row["Image ID"]
+            image__alt_text = row["Alt"] or row["Suggested"]
+
+            all_products_images[product__id].append({"id": image__id, "alt": image__alt_text})
+            line_count = i
+
+        if verbose:
+            console.print("[magenta bold]Updating images for the following product IDs")
+            console.print(dict(all_products_images))
+
+        if write:
+            for product_id, product_images in all_products_images.items():
+                wc_update_product_images(product_id, product_images)
+        print(f'Processed {line_count} lines.')
